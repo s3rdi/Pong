2903c7602c868ddba8cfcfa1882f2c7e9c29a5dc
serdi <serdi@cyberia.pl>
2023-08-05 15:07:16 +0200

    Added basic points count

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
-﻿#include <SFML/Graphics.hpp>
+﻿#include <iostream>
+#include <SFML/Graphics.hpp>
 #include "Platform.h"
 #include "Ball.h"
 
 namespace Config {
-    //height of user's view
-    static constexpr float g_viewHeight{ 1024.0f };
+    //user's view size
+    static constexpr float viewWidth{ 1280.0f };
+    static constexpr float viewHeight{ 720.0f };
 
     //max lives for both players
     static constexpr int maxLives{ 3 };
@@ -14,37 +16,60 @@ namespace Config {
 
     //bar config
     static const sf::Vector2f barSize{ 20.0f, 200.0f };
-    static constexpr float barPosition{ 400.0f };
+    static constexpr float barPosition{ 450.0f };
+
+    //text config
+    static constexpr int textSize{ 50 };
+    static constexpr float textMargin{ 10.0f };
+    static constexpr float textAxisX{ 50.0f };
 
     //game color
-    static const sf::Color gameColor{ sf::Color::White };
+    static const sf::Color propsColor{ sf::Color::White };
+    static const sf::Color bgColor{ sf::Color::Black };
+
 }
 
 //scaling the game to user resized UI
 void resizeView(const sf::RenderWindow& window, sf::View& view) {
     float aspectRatio{ static_cast<float>(window.getSize().x) / static_cast<float>(window.getSize().y) };
-    view.setSize(sf::Vector2f(Config::g_viewHeight * aspectRatio, Config::g_viewHeight));
+    view.setSize(sf::Vector2f(Config::viewHeight * aspectRatio, Config::viewHeight));
 }
 
 int main()
 {
-    //creating window and view
-    sf::RenderWindow window(sf::VideoMode(1024, 1024), "Pong", sf::Style::Default);
-    sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(1024.0f, Config::g_viewHeight));
+    //creating window view and font
+    sf::RenderWindow window(sf::VideoMode(static_cast<unsigned int>(Config::viewWidth), static_cast<unsigned int>(Config::viewHeight)), "Pong", sf::Style::Default);
+    sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(Config::viewWidth, Config::viewHeight));
+    sf::Font font{};
+
+    //handling font loading
+    if (!font.loadFromFile("./fonts/Rushbold.ttf")) {
+        std::cout << "Failed to load the font!\n";
+        return EXIT_FAILURE;
+    }
 
     //creating objects
-    Platform userBar(Config::gameColor, Config::barSize, sf::Vector2f(-Config::barPosition, 0.0f));
-    Platform enemyBar(Config::gameColor, Config::barSize, sf::Vector2f(Config::barPosition, 0.0f));
-    Ball ball(Config::gameColor, Config::ballRadius, sf::Vector2f(ConfigB::startPositionX, ConfigB::startPositionY));
+    Platform userBar(Config::propsColor, Config::barSize, sf::Vector2f(-Config::barPosition, 0.0f));
+    Platform enemyBar(Config::propsColor, Config::barSize, sf::Vector2f(Config::barPosition, 0.0f));
+    Ball ball(Config::propsColor, Config::ballRadius, sf::Vector2f(ConfigB::startPositionX, ConfigB::startPositionY));
     int userLives{ Config::maxLives };
     int enemyLives{ Config::maxLives };
     bool waitForInput{ false };
 
+    //creating text
+    sf::Text userLivesT{std::to_string(Config::maxLives), font, Config::textSize};
+    userLivesT.setFillColor(Config::propsColor);
+    userLivesT.setOrigin(userLivesT.getGlobalBounds().getSize().x, 0);
+    userLivesT.setPosition(-Config::textAxisX, -Config::viewHeight / 2.0f + Config::textMargin);
+    sf::Text enemyLivesT{std::to_string(Config::maxLives), font, Config::textSize};
+    enemyLivesT.setFillColor(Config::propsColor);
+    enemyLivesT.setPosition(Config::textAxisX, -Config::viewHeight / 2.0f + Config::textMargin);
+
     //game running
     while (window.isOpen()) {
-        sf::Event evnt;
+        sf::Event evnt{};
 
-        //handling user calls
+        //handling all events
         while (window.pollEvent(evnt)) {
             switch (evnt.type) {
             case sf::Event::Closed:
@@ -54,6 +79,7 @@ int main()
                 resizeView(window, view);
             }
         }
+
 
         //handling ball movement
         while (waitForInput) {
@@ -66,6 +92,8 @@ int main()
         ball.collidingBar(userBar, enemyBar);
         ball.screenCollision(view, userLives, enemyLives, waitForInput);
         ball.update();
+        userLivesT.setString(std::to_string(userLives));
+        enemyLivesT.setString(std::to_string(enemyLives));
 
         //handling AI movement
         enemyBar.moveAI(ball, enemyBar);
@@ -76,8 +104,10 @@ int main()
         userBar.handleCollision(view);
 
         //handling the window output
-        window.clear(sf::Color::Black);
+        window.clear(Config::bgColor);
         window.setView(view);
+        window.draw(userLivesT);
+        window.draw(enemyLivesT);
         userBar.draw(window);
         enemyBar.draw(window);
         ball.draw(window);
